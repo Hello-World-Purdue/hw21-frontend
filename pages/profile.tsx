fa3fcb70f3c2aea2b22170102f73bb9820dc454d
Shayne Marques <marques0@purdue.edu>
2021-06-26 19:42:27 -0400

    Added background

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,6 +1,36 @@
+import styles from "../styles/Home.module.css";
+
 export default function Profile() {
   return (
-    <div>
+    <div className={styles.profile_main}>
+      <h1>Profile</h1>
+      <h1>Name</h1>
+      <a href="/review">
+        <h2>My Application</h2>
+      </a>
+      <a>
+        <h2>View Application Status</h2>
+      </a>
+      <a href="/edit">
+        <h2>Edit Application</h2>
+      </a>
+      <a href="/wallet">
+        <h2>Wallet Access Link</h2>
+      </a>
+      <h1>Profile</h1>
+      <h1>Name</h1>
+      <a href="/review">
+        <h2>My Application</h2>
+      </a>
+      <a>
+        <h2>View Application Status</h2>
+      </a>
+      <a href="/edit">
+        <h2>Edit Application</h2>
+      </a>
+      <a href="/wallet">
+        <h2>Wallet Access Link</h2>
+      </a>
       <h1>Profile</h1>
       <h1>Name</h1>
       <a href="/review">
