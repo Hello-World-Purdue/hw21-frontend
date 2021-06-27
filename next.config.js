80d61fdf90945e97f129dde973a3128b5ec0bf05
Shayne Marques <marques0@purdue.edu>
2021-06-27 14:30:23 -0400

    Added next js module for images

--- next.config.js
+++ next.config.js
@@ -1,26 +1,3 @@
 const webpack = require("webpack");
-
-module.exports = {
-  entry: "./src/index.js",
-  module: {
-    rules: [
-      //...
-      {
-        test: /\.(png|jp(e*)g|svg|gif)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "images/[hash]-[name].[ext]",
-            },
-          },
-        ],
-      },
-      {
-        test: /\.svg$/,
-        use: ["@svgr/webpack"],
-      },
-    ],
-  },
-  //...
-};
+const withImages = require("next-images");
+module.exports = withImages();
