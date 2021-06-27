d312c7f4204b4961667491897860b4b1b6c7e92c
Shayne Marques <marques0@purdue.edu>
2021-06-26 23:11:45 -0400

    Changed edit page and button

--- pages/edit.tsx
+++ pages/edit.tsx
@@ -1,7 +1,29 @@
+import styles from "../styles/Home.module.css";
+import ProfileInfo from "../Components/profile/ProfileInfo";
+
 export default function Edit() {
   return (
-    <div>
-      <h1>Edit Application</h1>
+    <div className={styles.profile_main}>
+      <div className={styles.profile_box}>
+        <ProfileInfo
+          image="../../images/profile/profile_face.svg"
+          header="Full Name"
+          placeholder="First Last"
+        />
+        <ProfileInfo
+          image="../../images/profile/profile_email.svg"
+          header="Email"
+          placeholder="firstlast@purdue.edu"
+        />
+        <ProfileInfo
+          image="../../images/profile/profile_status.svg"
+          header="Application Status"
+          placeholder="Pending"
+        />
+        <div className={styles.profile_center}>
+          <button className={styles.profile_button}>SUBMIT</button>
+        </div>
+      </div>
     </div>
   );
 }
