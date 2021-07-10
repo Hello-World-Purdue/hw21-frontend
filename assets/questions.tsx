44a9cb7b0ad1f69db2eae7255786b556cd2d0e28
Shayne Marques <marques0@purdue.edu>
2021-07-10 17:52:49 -0400

    Created faq from list with css

--- assets/questions.tsx
+++ assets/questions.tsx
@@ -120,3 +120,66 @@ const questions = [
     a: ["September 13"],
   },
 ];
+
+const getQuestions = (list) => {
+  let content = [];
+  for (let i = 0; i < list.length; i += 3) {
+    content.push(
+      <>
+        <div
+          className={styles.faq_question}
+          style={{ backgroundColor: "rgb(255,0,9)", color: "white" }}
+        >
+          <p className={styles.faq_questionText}>{list[i]["q"]}</p>
+        </div>
+        <div className={styles.faq_answer}>{getAnswers(list[i]["a"])}</div>
+        {i + 1 < list.length && (
+          <div
+            className={styles.faq_question}
+            style={{ backgroundColor: "rgb(0,141,179) ", color: "white" }}
+          >
+            <p className={styles.faq_questionText}>{list[i + 1]["q"]}</p>
+          </div>
+        )}
+        {i + 1 < list.length && (
+          <div className={styles.faq_answer}>
+            {getAnswers(list[i + 1]["a"])}
+          </div>
+        )}
+        {i + 2 < list.length && (
+          <div
+            className={styles.faq_question}
+            style={{ backgroundColor: "rgb(255,230,43)", color: "black" }}
+          >
+            <p className={styles.faq_questionText}>{list[i + 2]["q"]}</p>
+          </div>
+        )}
+        {i + 2 < list.length && (
+          <div className={styles.faq_answer}>
+            {getAnswers(list[i + 2]["a"])}
+          </div>
+        )}
+      </>
+    );
+  }
+  return content;
+};
+
+const getAnswers = (list) => {
+  let content = [];
+  if (list.length == 1) {
+    content.push(<p className={styles.faq_answerText}>{list[0]}</p>);
+  } else {
+    content.push(
+      <ul className={styles.faq_answerList}>
+        {list.map((item) => (
+          <li className={styles.faq_answerText}>{item}</li>
+        ))}
+      </ul>
+    );
+  }
+  return content;
+};
+
+const faqList = getQuestions(questions);
+export default faqList;
--- pages/faq.tsx
+++ pages/faq.tsx
@@ -1,12 +1,17 @@
 import React, { FC } from "react";
+import styles from "../styles/Home.module.css";
 
 import Link from "next/link";
-import { Layout } from "../Components/Layout";
+import faqList from "../assets/questions";
 
-//import 'bootstrap/dist/css/bootstrap.css';
-import styles from "../styles/Home.module.css";
 export default function Faq() {
-  return(
-    <Layout></Layout>
-  )
+  return (
+    <div className={styles.faq_main}>
+      <div className={styles.faq_header}>
+        <h1 className={styles.faq_heading}>FAQ</h1>
+        <h3 className={styles.faq_description}>FREQUENTLY ASKED QUESTIONS</h3>
+      </div>
+      <div className={styles.faq_content}>{faqList}</div>
+    </div>
+  );
 }
