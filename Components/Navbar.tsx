c7ba9899d44ff22d4a3c81300b3f5fa806a20725
Victoire <victoire.beaufil@gmail.com>
2021-07-26 14:52:46 -0400

    Changed bootstrap hamburger menu icon to custom one

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -3,25 +3,43 @@ import React, { FC } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
-//import 'bootstrap/dist/css/bootstrap.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+
 import styles from "../styles/Home.module.css";
+
+
 
 type NavbarProps = {};
 
 export const Navbar: FC<NavbarProps> = () => {
+  function toggleNavbar() {
+    
+  }
+
+
   return (
     <div className={styles.topNav}>
+      <nav className="navbar navbar-expand-lg navbar-dark">
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon "></span>
+    </button>
       {/* className was previously styles.topNav*/}
-      <ul>
+     
       {/* We'll deal with active pages after */}
       {/* <a className={styles.active} href="/">Home</a> */}
-      <a href="/schedule">Schedule</a>
-      <a href="/announcements">Announcements</a>
-
-      <a href="/faq">FAQ's</a>
+      <div className="collapse navbar-collapse" id="navbar">
+      <ul className="navbar-nav">
+      <li className="nav-item"><a className="nav-link" href="/schedule">Schedule</a></li>
+      <li className="nav-item"><a className="nav-link" href="/announcements">Announcements</a></li>
+      <li className="nav-item"><a className="nav-link" href="/faq">FAQ's</a></li>
+      </ul>
+      </div>
       {/* Find a way to center big old logo img vertically */}
       {/* the issue of vertical alignment only happens when I wrap the img with an <a>*/}
-      <a href="/">
+     
+      <a className="navbar-brand d-flex" href="/">
+        
         <Image
           src="/logo.png"
           layout="intrinsic"
@@ -30,26 +48,30 @@ export const Navbar: FC<NavbarProps> = () => {
         ></Image>
       </a>
 
-      <Link href="/resources">
-        <a>Resources</a>
-      </Link>
-      <a href="/sponsors">Sponsors</a>
-
+      
+      <div className="collapse navbar-collapse" id="navbar">
+      <ul className="navbar-nav">
+      <li className="nav-item"> <a  className="nav-link" href="/resources">Resources
+       </a></li>
+       <li className="nav-item"><a  className="nav-link" href="/sponsors">Sponsors</a></li>
       {/* IF ADMIN */}
       <div className={styles.dropdown}>
         <button className={styles.dropbtn}>Manage</button>
         <div className={styles.dropdownContent}>
-          <Link href="/admin_dashboard">
-            <a>Admin Dashboard</a>
-          </Link>
-          <Link href="#">
-            <a>Announcements</a>
-          </Link>
+        <li className="nav-item">  <a  className="nav-link" href="/admin_dashboard">Admin Dashboard</a></li>
+        <li className="nav-item"> <a className="nav-link" href="#">
+            Announcements
+            </a> </li>
+         
         </div>
       </div>
       {/* ELSE */}
-      <button className="login-register-button"> LOGIN/REGISTER</button>
       </ul>
+      </div>
+      <button className="login-register-button "> LOGIN/REGISTER</button>
+   
+      </nav>
     </div>
+
   );
 };
