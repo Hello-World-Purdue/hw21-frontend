d23b499f7e84fac7a07adfc338e5a1afaeaa1da1
Victoire <victoire.beaufil@gmail.com>
2021-07-26 14:37:52 -0400

    Added custom hamburger menu icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import '../styles/globals.css'
 import '../styles/index.css'
 import '../styles/navbar.css'
-
 import '../styles/index.module.scss'
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
