38d043827053a579c86bbd8404a5b3779dcd0127
Shayne Marques <marques0@purdue.edu>
2021-08-01 17:13:53 -0400

    Fixed nav bar spacing

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -13,42 +13,37 @@ export const Navbar: FC<NavbarProps> = () => {
     <div className={styles.topNav}>
       {/* className was previously styles.topNav*/}
       <ul>
-      {/* We'll deal with active pages after */}
-      {/* <a className={styles.active} href="/">Home</a> */}
-      <a href="/schedule">Schedule</a>
-      <a href="/announcements">Announcements</a>
+        {/* We'll deal with active pages after */}
+        {/* <a className={styles.active} href="/">Home</a> */}
+        <a href="/schedule">Schedule</a>
+        <a href="/announcements">Announcements</a>
 
-      <a href="/faq">FAQ's</a>
-      {/* Find a way to center big old logo img vertically */}
-      {/* the issue of vertical alignment only happens when I wrap the img with an <a>*/}
-      <a href="/">
-        <Image
-          src="/logo.png"
-          layout="intrinsic"
-          width={50}
-          height={50}
-        ></Image>
-      </a>
+        <a href="/faq">FAQ's</a>
+        {/* Find a way to center big old logo img vertically */}
+        {/* the issue of vertical alignment only happens when I wrap the img with an <a>*/}
+        <a href="/">
+          <Image src="/logo.png" width={50} height={50}></Image>
+        </a>
 
-      <Link href="/resources">
-        <a>Resources</a>
-      </Link>
-      <a href="/sponsors">Sponsors</a>
+        <Link href="/resources">
+          <a>Resources</a>
+        </Link>
+        <a href="/sponsors">Sponsors</a>
 
-      {/* IF ADMIN */}
-      <div className={styles.dropdown}>
-        <button className={styles.dropbtn}>Manage</button>
-        <div className={styles.dropdownContent}>
-          <Link href="/admin_dashboard">
-            <a>Admin Dashboard</a>
-          </Link>
-          <Link href="#">
-            <a>Announcements</a>
-          </Link>
+        {/* IF ADMIN */}
+        <div className={styles.dropdown}>
+          <button className={styles.dropbtn}>Manage</button>
+          <div className={styles.dropdownContent}>
+            <Link href="/admin_dashboard">
+              <a>Admin Dashboard</a>
+            </Link>
+            <Link href="#">
+              <a>Announcements</a>
+            </Link>
+          </div>
         </div>
-      </div>
-      {/* ELSE */}
-      <button className="login-register-button"> LOGIN/REGISTER</button>
+        {/* ELSE */}
+        <button className="login-register-button"> LOGIN/REGISTER</button>
       </ul>
     </div>
   );
