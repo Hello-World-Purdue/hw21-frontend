a8efa4c77e1ad11c52d07c713c9cf2fb863023f0
Shayne Marques <marques0@purdue.edu>
2021-08-01 16:05:59 -0400

    Added footer and fixed position

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from "react";
 import styles from "../styles/Home.module.css";
 
-import Link from "next/link";
 import { Header } from "../Components/Header";
 import faqList from "../assets/questions";
 import { Layout } from "../Components/Layout";
@@ -12,56 +11,60 @@ const submitHandler = (e) => {
 
 export default function Faq() {
   return (
-    <div className="faq">
-      <Header headerImgClass="faq-header">
-        <div className={styles.header_text}>
-          <h1 className={styles.header_heading}>FAQ</h1>
-          <p className={styles.header_description}>
-            FREQUENTLY ASKED QUESTIONS
-          </p>
-        </div>
-      </Header>
-      <div className={styles.faq_main}>
-        <div className={styles.faq_content}>{faqList}</div>
-        <div className={styles.faq_other}>
-          <div className={styles.faq_otherHeader}>
-            <p className={styles.faq_otherHeading}>HAVE ANY OTHER QUESTIONS?</p>
-            <p className={styles.faq_otherDescription}>
-              ASK IT HERE OR CONTACT US ON OUR SOCIAL MEDIA
+    <Layout>
+      <div className="faq">
+        <Header headerImgClass="faq-header">
+          <div className={styles.header_text}>
+            <h1 className={styles.header_heading}>FAQ</h1>
+            <p className={styles.header_description}>
+              FREQUENTLY ASKED QUESTIONS
             </p>
           </div>
-          <form>
-            <div className={styles.faq_formContainer}>
-              <div className={styles.faq_inputContainer}>
-                <input
-                  className={styles.faq_input}
-                  type="text"
-                  name="name"
-                  id="name"
-                  placeholder="YOUR NAME"
-                />
-                <input
-                  className={styles.faq_input}
-                  type="email"
-                  name="email"
-                  id="email"
-                  placeholder="EMAIL ADDRESS"
-                />
+        </Header>
+        <div className={styles.faq_main}>
+          <div className={styles.faq_content}>{faqList}</div>
+          <div className={styles.faq_other}>
+            <div className={styles.faq_otherHeader}>
+              <p className={styles.faq_otherHeading}>
+                HAVE ANY OTHER QUESTIONS?
+              </p>
+              <p className={styles.faq_otherDescription}>
+                ASK IT HERE OR CONTACT US ON OUR SOCIAL MEDIA
+              </p>
+            </div>
+            <form>
+              <div className={styles.faq_formContainer}>
+                <div className={styles.faq_inputContainer}>
+                  <input
+                    className={styles.faq_input}
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="YOUR NAME"
+                  />
+                  <input
+                    className={styles.faq_input}
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="EMAIL ADDRESS"
+                  />
+                </div>
+                <textarea className={styles.faq_textArea}></textarea>
               </div>
-              <textarea className={styles.faq_textArea}></textarea>
-            </div>
-            <div className={styles.faq_submitContainer}>
-              <button
-                onClick={submitHandler}
-                className={styles.faq_submit}
-                type="submit"
-              >
-                SUBMIT
-              </button>
-            </div>
-          </form>
+              <div className={styles.faq_submitContainer}>
+                <button
+                  onClick={submitHandler}
+                  className={styles.faq_submit}
+                  type="submit"
+                >
+                  SUBMIT
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </Layout>
   );
 }
