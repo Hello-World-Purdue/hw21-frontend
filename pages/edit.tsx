67aafaaf8065cb8bbe93d045f47da416565e7fc5
Shayne Marques <marques0@purdue.edu>
2021-08-02 13:25:20 -0400

    Completed profile page and faq page frontend

--- pages/edit.tsx
+++ pages/edit.tsx
@@ -1,6 +1,7 @@
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
 import { useState } from "react";
+import { Layout } from "../Components/Layout";
 
 export default function Edit() {
   const [email, setEmail] = useState("");
@@ -11,68 +12,74 @@ export default function Edit() {
     console.log(email);
   };
   return (
-    <div className={styles.profile_main}>
-      <form onSubmit={handleSubmit} className={styles.profile_box}>
-        <Image
-          src={require("../images/profile/profile_account.svg")}
-          className={styles.profile_account}
-          width="90%"
-          height="90%"
-        ></Image>
+    <Layout>
+      <div className={styles.profile_main}>
+        <form onSubmit={handleSubmit} className={styles.profile_box}>
+          <div className={styles.profile_account}>
+            <Image
+              src={require("../images/profile/profile_account.svg")}
+              layout="fill"
+            />
+          </div>
 
-        <div className={styles.profile_info}>
-          <img
-            alt="full name"
-            src={require("../images/profile/profile_face.svg")}
-          />
-          <div className={styles.profile_container}>
-            <div className={styles.profile_header}>
-              <h1>Full Name</h1>
-            </div>
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className={styles.profile_input}
-              placeholder="First Last"
-            ></input>
-          </div>
-        </div>
-        <div className={styles.profile_info}>
-          <img
-            alt="email"
-            src={require("../images/profile/profile_email.svg")}
-          />
-          <div className={styles.profile_container}>
-            <div className={styles.profile_header}>
-              <h1>Email</h1>
-            </div>
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className={styles.profile_input}
-              placeholder="firstlast@purdue.edu"
-            ></input>
-          </div>
-        </div>
-        <div className={styles.profile_info}>
-          <img
-            className={styles.profile_image}
-            alt="status"
-            src={require("../images/profile/profile_status.svg")}
-          />
-          <div className={styles.profile_container}>
-            <div className={styles.profile_header}>
-              <h1>Application Status</h1>
-            </div>
-            <div className={styles.profile_data}>
-              <h1>Pending</h1>
+          <div className={styles.profile_info}>
+            <img
+              alt="full name"
+              src={require("../images/profile/profile_face.svg")}
+            />
+            <div className={styles.profile_container}>
+              <div className={styles.profile_header}>
+                <h1>Full Name</h1>
+              </div>
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className={styles.profile_input}
+                placeholder="First Last"
+              ></input>
             </div>
           </div>
-        </div>
-        <input type="submit" value="SUBMIT" className={styles.profile_button} />
-      </form>
-    </div>
+          <div className={styles.profile_info}>
+            <img
+              alt="email"
+              src={require("../images/profile/profile_email.svg")}
+            />
+            <div className={styles.profile_container}>
+              <div className={styles.profile_header}>
+                <h1>Email</h1>
+              </div>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className={styles.profile_input}
+                placeholder="firstlast@purdue.edu"
+              ></input>
+            </div>
+          </div>
+          <div className={styles.profile_info}>
+            <img
+              className={styles.profile_image}
+              alt="status"
+              src={require("../images/profile/profile_status.svg")}
+            />
+            <div className={styles.profile_container}>
+              <div className={styles.profile_header}>
+                <h1>Application Status</h1>
+              </div>
+              <div className={styles.profile_data}>
+                <h1>Pending</h1>
+              </div>
+            </div>
+          </div>
+          <input
+            type="submit"
+            value="SUBMIT"
+            className={styles.profile_button}
+          />
+        </form>
+      </div>
+    </Layout>
   );
 }
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,6 +1,7 @@
 import styles from "../styles/Home.module.css";
 import ProfileInfo from "../Components/profile/ProfileInfo";
 import Image from "next/image";
+import { Layout } from "../Components/Layout";
 
 const onEditButtonClicked = () => {
   location.href = "/edit";
@@ -8,21 +9,26 @@ const onEditButtonClicked = () => {
 
 export default function Profile() {
   return (
-    <div className={styles.profile_main}>
-      <div className={styles.profile_box}>
-        <Image
-          src={require("../images/profile/profile_account.svg")}
-          className={styles.profile_account}
-          width="90%"
-          height="90%"
-        />
-        <ProfileInfo header="Full Name" placeholder="First Last" />
-        <ProfileInfo header="Email" placeholder="firstlast@purdue.edu" />
-        <ProfileInfo header="Application Status" placeholder="Pending" />
-        <button onClick={onEditButtonClicked} className={styles.profile_button}>
-          EDIT PROFILE
-        </button>
+    <Layout>
+      <div className={styles.profile_main}>
+        <div className={styles.profile_box}>
+          <div className={styles.profile_account}>
+            <Image
+              src={require("../images/profile/profile_account.svg")}
+              layout="fill"
+            />
+          </div>
+          <ProfileInfo header="Full Name" placeholder="First Last" />
+          <ProfileInfo header="Email" placeholder="firstlast@purdue.edu" />
+          <ProfileInfo header="Application Status" placeholder="Pending" />
+          <button
+            onClick={onEditButtonClicked}
+            className={styles.profile_button}
+          >
+            EDIT PROFILE
+          </button>
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 }
