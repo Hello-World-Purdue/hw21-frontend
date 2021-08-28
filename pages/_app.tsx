46dc77b76783308fc7d8580919fe71c3c0c839e5
Victoire <victoire.beaufil@gmail.com>
2021-08-28 18:08:19 -0400

    Added Popper.js and Bootstrap <script> to _app.tsx, fixed Navbar Manage dropdown for small devices

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,27 @@
-<<<<<<< HEAD
-import 'bootstrap/dist/css/bootstrap.min.css';
+// Import head to add <script> tag to every page for Bootstrap JS and Popper.JS
+import Head from 'next/head'
 
-import '../styles/globals.css'
-import '../styles/index.css'
-import '../styles/navbar.css'
-import '../styles/index.module.scss'
-=======
 import { AuthContextProvider } from "../context/AuthContext";
 import { UserContextProvider } from "../context/UserContext";
 import { AlertContextProvider } from "../context/AlertContext";
 
 import Alert from "../Components/Alert";
 
-import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
 import "../styles/index.css";
 import "../styles/navbar.css";
 
 import "../styles/index.module.css";
->>>>>>> 4d8ec26eaf12a17d7cde866136e0f5e0e308384b
 function MyApp({ Component, pageProps }) {
 	return (
 		<AuthContextProvider>
 			<UserContextProvider>
 				<AlertContextProvider>
+				<Head>
+        <script async src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossOrigin="anonymous" />
+		<script async src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossOrigin="anonymous" />
+      </Head>
 					<Component {...pageProps} />
 					<Alert />
 				</AlertContextProvider>
