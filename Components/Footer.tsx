1136e6aa0e6d70c521271eb537cd938fc35a01fc
aakarshitpandey <aakarshit.pandey7@gmail.com>
2021-09-03 16:01:02 -0400

    fixed footer

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -16,7 +16,7 @@ export const Footer: FC<FooterProps> = () => {
         {/* className was previously styles.topNav*/}
 
         {/* We'll deal with active pages after */}
-        <a href="/schedule">Home</a>
+        <a href="/">Home</a>
         {/* <a className={styles.active} href="/">Home</a> */}
         <a href="/schedule">Schedule</a>
         <a href="/announcements">Announcements</a>
@@ -28,10 +28,10 @@ export const Footer: FC<FooterProps> = () => {
         <a href="/sponsors">Sponsors</a>
       </div>
       <div className="footer-social-media">
-        <a href="https://www.facebook.com/helloworldpurdue/">
+        <a href="https://www.facebook.com/helloworldpurdue/" target="_blank" style={{margin: '10px'}}>
           <Image src="/facebook.png" height={30} width={30}></Image>
         </a>
-        <a href="https://www.instagram.com/helloworldpurdue/?hl=en">
+        <a href="https://www.instagram.com/helloworldpurdue/?hl=en" target="_blank" style={{margin: '10px'}}>
           <Image src="/instagram.png" height={30} width={30}></Image>
         </a>
       </div>
