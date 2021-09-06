bdebefb1016b4d29f876d8afc2a1c8055c3a1dc9
aakarshitpandey <aakarshit.pandey7@gmail.com>
2021-09-06 05:57:07 -0400

    added content to the sponsors page

--- pages/sponsors.tsx
+++ pages/sponsors.tsx
@@ -4,6 +4,8 @@ import styles from '../styles/Home.module.css'
 import React, { FC, Fragment } from 'react';
 import { Header } from '../Components/Header';
 import { Layout } from '../Components/Layout';
+import { sponsors } from '../util/sponsors';
+import Image from 'next/image'
 export default function Sponsors() {
   return (
     <div className={styles.resources}>
@@ -18,7 +20,28 @@ export default function Sponsors() {
                 <p className={styles.header_description}>SEE WHAT OUR SPONSORS HAVE TO OFFER</p>
                 </div> */}
        </Header>
-    
+       <div>
+        {
+          sponsors.map(sponsor => (
+            <div
+              key={sponsor.name}
+              className={styles.sponsor_card}
+              style={{ backgroundColor: sponsor.background, color: "white" }}
+            >
+              <h1>{sponsor.name}</h1>
+              <div className="" style={{textAlign: 'center'}}>
+              <Image
+                src={`/sponsors/${sponsor.name}_logo.png`}
+                layout="intrinsic"
+                width={sponsor.width / 2}
+                height={sponsor.height / 2}
+					    ></Image>
+              </div>
+              <p className={styles.faq_questionText}>{sponsor.about}</p>
+            </div>
+          ))
+        }    
+        </div>
       </Layout>
     </div>
   )
