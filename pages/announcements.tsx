9962527a7bdd589e54ed8a313584cb995e61e490
darreniyer0601 <54214698+darreniyer0601@users.noreply.github.com>
2021-09-06 16:34:32 -0400

    removed dummy announcements

--- pages/announcements.tsx
+++ pages/announcements.tsx
@@ -7,44 +7,44 @@ import { Layout } from "../Components/Layout";
 import { Header } from "../Components/Header";
 
 interface Announcement {
-  type: "events" | "food" | "judging" | "logistics" | "sponsor" | "misc";
-  message: string;
+	type: "events" | "food" | "judging" | "logistics" | "sponsor" | "misc";
+	message: string;
 }
 
-const list: Announcement[] = [
-  {
-    type: "food",
-    message: "LUNCH - AVAILABLE IN ROOM 3084",
-  },
-  {
-    type: "sponsor",
-    message: "SPONSOR EVENT - ROOM 2124",
-  },
-  {
-    type: "events",
-    message: "SMASH TOURNAMENT - ROOM 1140",
-  },
-  {
-    type: "judging",
-    message: "JUDGING WILL BEGIN IN 1 HOUR",
-  },
-];
+// const list: Announcement[] = [
+//   {
+//     type: "food",
+//     message: "LUNCH - AVAILABLE IN ROOM 3084",
+//   },
+//   {
+//     type: "sponsor",
+//     message: "SPONSOR EVENT - ROOM 2124",
+//   },
+//   {
+//     type: "events",
+//     message: "SMASH TOURNAMENT - ROOM 1140",
+//   },
+//   {
+//     type: "judging",
+//     message: "JUDGING WILL BEGIN IN 1 HOUR",
+//   },
+// ];
 
 function Announcements() {
-  const [annList, setAnnList] = useState<Announcement[]>(list);
+	//   const [annList, setAnnList] = useState<Announcement[]>(list);
 
-  return (
-    <div className={styles.announcementContainer}>
-      <Layout>
-        <Header headerImgClass="announcements-header">
-          {/* <div className={homeStyles.header_text}>
+	return (
+		<div className={styles.announcementContainer}>
+			<Layout>
+				<Header headerImgClass="announcements-header">
+					{/* <div className={homeStyles.header_text}>
 						<h1 className={homeStyles.header_heading}>ANNOUNCEMENTS</h1>
 						<p className={homeStyles.header_description}>
 							KEEP YOURSELF UPDATED WITH WHAT'S HAPPENING
 						</p>
 					</div> */}
-        </Header>
-        <div className={styles.announcementList}>
+				</Header>
+				{/* <div className={styles.announcementList}>
           {annList.map((announcement) => {
             return (
               <div className={styles.announcement}>
@@ -53,10 +53,13 @@ function Announcements() {
               </div>
             );
           })}
-        </div>
-      </Layout>
-    </div>
-  );
+        </div> */}
+				<div style={{ textAlign: "center", marginBottom: '10vh' }}>
+					<h4 style={{ fontFamily: "backissues" }}>NO ANNOUNCEMENTS YET.</h4>
+				</div>
+			</Layout>
+		</div>
+	);
 }
 
 export default Announcements;
